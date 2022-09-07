783b69fb9b2788f77375eed11ad60bb0c78eb37b
Alex <alechenne@gmail.com>
2022-09-07 23:13:05 +0200

    Directives (new): 2 niveau impressions error, activation auto transition et transition RX->TX ou RX-RX, check cohérence entre directives

--- BiDir/app_init.h
+++ BiDir/app_init.h
@@ -45,24 +45,40 @@
 #define qMaster			0			// Slave
 #endif	// SL_MASTER
 
-#define qUseDisplay     1			// Display statistics
+#define qUseDisplay     0/*1*/			// Display statistics
 #define qPrintTX        0			// Print out sent data on serial COM
 #define qPrintRX        0			// Print out received data on serial COM
 #define qPrintEvents	1			// Print out events on serial COM
-#define qPrintErrors	0			// Print out RAIL errors on serial COM
+#define qPrintErrorsL1	1			// Print out RAIL errors on serial COM (generally return of RAIL_functions)
+#define qPrintErrorsL2	0			// Print out RAIL errors on serial COM (kTimeOutTx, kTimeOutRx, kErrorTx, kErrorRx)
+
+#define qAutoTransition 1			// 0 = Transition to IDLE  1 = On RX/TX success/no success, transition to RX
+#define qRx2TxAutoTransition 1		// 0 = On RX success, transition to RX 1 = On RX success, transition to TX
 
 #if (qMaster)
 #define qUseScheduleRx	1			// 0 = RAIL_StartRx, 1 = RAIL_ScheduleRx
 #else
 #define qUseScheduleRx	0			// 0 = RAIL_StartRx, 1 = RAIL_ScheduleRx
 #endif	// qMaster
-#define qUseScheduleTx	0			// 0 = RAIL_StartTx, 1 = RAIL_StartScheduledTx
+#define qUseScheduleTx	0			// 0 = RAIL_StartTx, 1 = RAIL_StartScheduledTx			// 	Don't use it in this applicaiton BiDir
+
 #if (qMaster)
 #define qUseTimeOutRx	0			// 0 = no timeout with RAIL_StartRx, 1 = use MultiTimer for timeout RAIL_StartRx
+#define qUseTimeOutTx	1			// 0 = no timeout with RAIL_StartTx, 1 = use MultiTimer for timeout RAIL_StartTx
 #else
 #define qUseTimeOutRx	0			// 0 = no timeout with RAIL_StartRx, 1 = use MultiTimer for timeout RAIL_StartRx
+#define qUseTimeOutTx	0			// 0 = no timeout with RAIL_StartTx, 1 = use MultiTimer for timeout RAIL_StartTx
 #endif	// qMaster
-#define qUseTimeOutTx	1			// 0 = no timeout with RAIL_StartTx, 1 = use MultiTimer for timeout RAIL_StartTx
+
+#if (qMaster && !qUseScheduleRx && !qUseTimeOutRx)
+	#error qUseTimeOutRx soit être activé sur le Master si qUseScheduleRx estv inactif
+#endif
+#if (qUseScheduleTx)
+	#error qUseScheduleTx pas supporté dans cette application (BiDir)
+#endif
+#if (qUseScheduleRx && qUseTimeOutRx)
+	#error qUseScheduleRx et qUseTimeOutRx sont mutuellement exclusifs
+#endif
 
 // GPIO debug
 #define DEBUG_PIN_TX    1         	// PB01 --> Exp Header 9
@@ -75,13 +91,15 @@
 // Timeout for Start RX and TX RAIL functions
 #if (qMaster)
 #define RX_TIMEOUT	(650U/*850U*/)					// in us
-#define TX_START	(80U)							// in us
+#define TX_START	(80U)							// in us	// Not used
 #define TX_TIMEOUT	(2.05*RX_TIMEOUT)				// in us
 #else	// !qMaster
 #define RX_TIMEOUT	(850U/*850U*/)					// in us
-#define TX_START	(80U)							// in us
+#define TX_START	(80U)							// in us	// Not used
 #define TX_TIMEOUT	(2.05*RX_TIMEOUT)				// in us
 #endif	// qMaster
+
+#define TRANSITION_TIMING_BESTOF	1
 
 // -----------------------------------------------------------------------------
 //                                Global Variables
