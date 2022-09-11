1d99cbd735ea39d1617211413ec9140f2ebc5b1c
Alex <alechenne@gmail.com>
2022-09-11 19:11:28 +0200

    [Slot Common] Extend AddrMap structure to add an enable info to indicatate if the device is active (ffor instance not bringing slave 2 in the network but keeping it in the table) and change slotTime to to slotPos (position) because slot are a multiple of the slot time base / Add new functions to get the number of Slaves, number of Master, the slot pos of a device and the last slot position

--- Slot_Common/common_config.c
+++ Slot_Common/common_config.c
@@ -14,21 +14,31 @@
 /**
  * @brief Adresses mapping for all devices
  */
+//PROT_AddrMap_t addr_table[ADDR_TRANSLATION_TABLE_SIZE] =
+//{
+//    /* ID                Pos    Addr    slot [us]         Is master   Name          */
+//	{0x385B44FFFEC085D3, 0,     255,    0,                true,       "MASTER\0"},
+//	{0x385b44fffec0862b, 1,     1,      (0*TIME_SLOT),    false,      "SLAVE\0"},
+//	{0x385b44fffe5f5af2, 2,     2,      (3*TIME_SLOT),    false,      "SLAVE\0"},
+//  {0x385b44fffe5f5b23, 3,     3,      (1*TIME_SLOT),    false,      "SLAVE\0"},
+//  {0x385b44fffec08638, 4,     4,      (2*TIME_SLOT),    false,      "SLAVE\0"}
+//};
+
 PROT_AddrMap_t addr_table[ADDR_TRANSLATION_TABLE_SIZE] =
 {
-    /* ID                Pos    Addr    slot [us]         Is master   Name          */
-	{0x385B44FFFEC085D3, 0,     255,    0,                true,       "MASTER\0"},
-	{0x385b44fffec0862b, 1,     1,      (0*TIME_SLOT),    false,      "SLAVE\0"},
-	{0x385b44fffe5f5af2, 2,     2,      (3*TIME_SLOT),    false,      "SLAVE\0"},
-    {0x385b44fffe5f5b23, 3,     3,      (1*TIME_SLOT),    false,      "SLAVE\0"},
-    {0x385b44fffec08638, 4,     4,      (2*TIME_SLOT),    false,      "SLAVE\0"}
+    /* ID                Pos    Eanble  Addr    Slot pos          Is master   Name          */
+    {0x385B44FFFEC085D3, 0,     true,   255,    0,                true,       "MASTER\0"},
+    {0x385b44fffec0862b, 1,     true,   1,      0,                false,      "SLAVE\0"},
+    {0x385b44fffe5f5b23, 2,     true,   3,      1,                false,      "SLAVE\0"},
+    {0x385b44fffec08638, 3,     true,   4,      2,                false,      "SLAVE\0"},
+    {0x385b44fffe5f5af2, 4,     false,  2,      3,                false,      "SLAVE\0"},
 };
 
 /**
  * Return the device internal address according to the unique ID.
  *
  * @param[in] device unique ID.
- * @return  device internal address or 255 (not found).
+ * @return  device internal address or 0 (not found).
  */
 uint8_t common_getAddr(uint64_t uniqueId)
 {
@@ -60,4 +70,65 @@ PROT_AddrMap_t* common_getConfig(uint64_t uniqueId)
 	}
 
 	return NULL;
+}
+
+/**
+ * Return the device time slot position according to the unique ID.
+ *
+ * @param[in] device unique ID.
+ * @return  device internal slot pos or -1 (not found).
+ *          Max 127 devices (limit MAX_NODE shall be below)
+ */
+int8_t common_getTimeSlotPos(uint64_t uniqueId)
+{
+    for (int i = 0; i < ADDR_TRANSLATION_TABLE_SIZE; i++)
+    {
+        if (addr_table[i].uinqueId == uniqueId)
+        {
+            return addr_table[i].slotPos;
+        }
+    }
+
+    return -1;
+}
+
+/**
+ * Return the max time slot position.
+ *
+ * @param[in] -
+ * @return  device internal slot pos or -1 (not found).
+ *          Max 127 devices (limit MAX_NODE shall be below)
+ */
+int8_t common_getMaxTimeSlotPos(void)
+{
+    uint8_t max = -1;
+
+    for (int i = 0; i < ADDR_TRANSLATION_TABLE_SIZE; i++)
+    {
+        max = (addr_table[i].slotPos > max ? addr_table[i].slotPos : max);
+    }
+
+    return max;
+}
+
+
+/**
+ * Return the number of device .
+ *
+ * @param[in] type MASTER = true, SLAVE = false.
+ * @return  number of enabled devices.
+ */
+uint8_t common_getNbrDeviceOfType(bool type)
+{
+    uint8_t count = 0;
+
+    for (int i = 0; i < ADDR_TRANSLATION_TABLE_SIZE; i++)
+    {
+        if ((addr_table[i].ismaster == type) && addr_table[i].enable)
+        {
+            count++;
+        }
+    }
+
+    return count;
 }
--- Slot_Common/common_config.h
+++ Slot_Common/common_config.h
@@ -16,6 +16,8 @@
 
 #define SLAVE_IN_SYSTEM (3)                     // Netowrk is composed of X slaves
 #define TIME_SLOT       (240U)                  // in us
+#define TIME_SLOT_MIN   (100U)                  // in us
+#define TIME_SLOT_MAX   (40000U)                // in us
 
 // Master cycle of collecting data from Slaves
 // Slot 0   : Master
@@ -23,10 +25,17 @@
 // Slot 1   : Slave 2
 // ...
 // Slot N-1 : Slave N
-#define SYNC_PERIOD  ((SLAVE_IN_SYSTEM*TIME_SLOT)+(uint32_t)(0.25f*(float)TIME_SLOT)/*(uint32_t)(0.1f*SEC)*/)             // in us, TODO Nb slave + 0.5 car Slave 1 et Master communique dans slot 0 mais ce dernier dure plus long, à contrôler / analyser
+#define SYNC_PERIOD_FACT     (0.25f)
+#define SYNC_PERIOD  ((SLAVE_IN_SYSTEM*TIME_SLOT)+(uint32_t)(SYNC_PERIOD_FACT*(float)TIME_SLOT)/*(uint32_t)(0.1f*SEC)*/)             // in us, TODO Nb slave + 0.5 car Slave 1 et Master communique dans slot 0 mais ce dernier dure plus long, à contrôler / analyser
+#define SYNC_PERIOD_FACT_MIN (0)        // in %
+#define SYNC_PERIOD_FACT_MAX (100)      // in %
+
 
 // In Slave, considering if no Sync from Master after 5 times SYNC_PERIOD that Master stop its process -> restart state machine from Slaves
-#define SYNC_TIMEOUT (5*SYNC_PERIOD)
+#define SYNC_TIMEOUT_NB (5)
+#define SYNC_TIMOUT_MIN (1)        // in nbre of SYNC_TIMEOUT
+#define SYNC_TIMOUT_MAX (20)       // in nbre of SYNC_TIMEOUT
+#define SYNC_TIMEOUT     (SYNC_TIMEOUT_NB*SYNC_PERIOD)
 
 #define SEC (1000000U)
 
@@ -36,6 +45,10 @@
 
 
 #define ADDR_TRANSLATION_TABLE_SIZE	(MAX_NODE)
+#if (ADDR_TRANSLATION_TABLE_SIZE > INT8_MAX)
+#error MAX_NODE shall be less than 127!
+#endif
+
 #define ADDR_INTERNAL_NAME_STRING_SIZE (10)
 
 /**
@@ -47,8 +60,9 @@ typedef struct
 {
     uint64_t uinqueId;                           // Unique device ID
     uint8_t posTab;                              // Position in tab (in order to handle Master ID and avoid tab of Master ID size)
+    bool enable;                                 // Is the device part of the network?
     uint8_t internalAddr;                        // Internal address
-    uint32_t slotTime;                           // Slot time in uS
+    uint32_t slotPos;                            // Slot position (will multiplied by SLOT_TIME to get the TX scheduled slot time)
     bool ismaster;                               // true if master device
     char name[ADDR_INTERNAL_NAME_STRING_SIZE];   // Device name
 } PROT_AddrMap_t;
@@ -68,5 +82,35 @@ uint8_t common_getAddr(uint64_t uniqueId);
  * @return  device internal info data or NULL (not found).
  */
 PROT_AddrMap_t* common_getConfig(uint64_t uniqueId);
+
+/**
+ * Return the device time slot position according to the unique ID.
+ *
+ * @param[in] device unique ID.
+ * @return  device internal slot pos or -1 (not found).
+ *          Max 127 devices (limit MAX_NODE shall be below)
+ */
+int8_t common_getTimeSlotPos(uint64_t uniqueId);
+
+/**
+ * Return the max time slot position.
+ *
+ * @param[in] -
+ * @return  device internal slot pos or -1 (not found).
+ *          Max 127 devices (limit MAX_NODE shall be below)
+ */
+int8_t common_getMaxTimeSlotPos(void);
+
+
+/**
+ * Return the number of device .
+ *
+ * @param[in] type MASTER = true, SLAVE = false.
+ * @return  number of enabled devices.
+ */
+#define MASTER_TYPE (true)
+#define SLAVE_TYPE (false)
+
+uint8_t common_getNbrDeviceOfType(bool type);
 
 #endif /* PROT_ADDRESS_H_ */
