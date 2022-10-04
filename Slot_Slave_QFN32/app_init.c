cf72ef5b8e06c401f03ea3285012a9c30584bdc9
Alex <alechenne@gmail.com>
2022-10-04 11:10:53 +0200

    [Slot Slave QFN32] Update project file / remove include no more needed with IOStream SWO

--- Slot_Slave_QFN32/app_init.c
+++ Slot_Slave_QFN32/app_init.c
@@ -46,7 +46,6 @@
 // ---------------------
 #include "em_system.h"              // System functions
 #include "em_msc.h"                 // Memory access functions
-#include "em_cmu.h"
 #include "nvm3.h"                   // NVM Flash Memory functions
 #include "nvm3_hal_flash.h"         // NVM Flash Memory instance
 #include "printf.h"                 // Tiny printf
@@ -542,60 +541,11 @@ void graphics_init(void)
 #endif // SL_BOARD_ENABLE_DISPLAY
 }
 
-void setupSWOForPrint(void)
-{
-//  /* Enable GPIO clock. */
-//  CMU->HFPERCLKEN0 |= CMU_HFPERCLKEN0_GPIO;
-//
-//  /* Enable Serial wire output pin */
-//  GPIO->ROUTE |= GPIO_ROUTE_SWOPEN;
-//
-//#if defined(_EFM32_GIANT_FAMILY) || defined(_EFM32_LEOPARD_FAMILY) || defined(_EFM32_WONDER_FAMILY)
-//  /* Set location 0 */
-//  GPIO->ROUTE = (GPIO->ROUTE & ~(_GPIO_ROUTE_SWLOCATION_MASK)) | GPIO_ROUTE_SWLOCATION_LOC0;
-//
-//  /* Enable output on pin - GPIO Port F, Pin 2 */
-//  GPIO->P[5].MODEL &= ~(_GPIO_P_MODEL_MODE2_MASK);
-//  GPIO->P[5].MODEL |= GPIO_P_MODEL_MODE2_PUSHPULL;
-//#else
-//  /* Set location 1 */
-//  GPIO->ROUTE = (GPIO->ROUTE & ~(_GPIO_ROUTE_SWLOCATION_MASK)) |GPIO_ROUTE_SWLOCATION_LOC1;
-//  /* Enable output on pin */
-//  GPIO->P[2].MODEH &= ~(_GPIO_P_MODEH_MODE15_MASK);
-//  GPIO->P[2].MODEH |= GPIO_P_MODEH_MODE15_PUSHPULL;
-//#endif
-//
-//  /* Enable debug clock AUXHFRCO */
-//  CMU->OSCENCMD = CMU_OSCENCMD_AUXHFRCOEN;
-//
-//  /* Wait until clock is ready */
-//  while (!(CMU->STATUS & CMU_STATUS_AUXHFRCORDY));
-//
-//  /* Enable trace in core debug */
-//  CoreDebug->DEMCR |= CoreDebug_DEMCR_TRCENA_Msk;
-//  ITM->LAR  = 0xC5ACCE55;
-//  ITM->TER  = 0x0;
-//  ITM->TCR  = 0x0;
-//  TPI->SPPR = 2;
-//  TPI->ACPR = 0xf;
-//  ITM->TPR  = 0x0;
-//  DWT->CTRL = 0x400003FE;
-//  ITM->TCR  = 0x0001000D;
-//  TPI->FFCR = 0x00000100;
-//  ITM->TER  = 0x1;
-}
-
 /*******************************************************************************
  * @brief Print out welcome info on serial COM.
  ******************************************************************************/
 void serial_init(void)
 {
-    const char* str ="Hello\n";
-    setupSWOForPrint();
-
-    for (int i = 0; i < sizeof(str); i++)
-        ITM_SendChar(str[i]);
-
     // Print Id software
     char string[80] = "\nSlot Protocol";
 
