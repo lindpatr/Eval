d16ed0fee1a95ac89680fac6867aa888f350f49c
Alex <alechenne@gmail.com>
2022-10-04 12:56:28 +0200

    [Slot Master] PTI in UART mode for testing purposes (Network Analyzer)

--- Slot_Master/config/pin_config.h
+++ Slot_Master/config/pin_config.h
@@ -82,10 +82,6 @@
 #define USART0_CLK_PORT                          gpioPortC
 #define USART0_CLK_PIN                           2
 
-// USART0 CS on PC03
-#define USART0_CS_PORT                           gpioPortC
-#define USART0_CS_PIN                            3
-
 // USART0 RX on PC01
 #define USART0_RX_PORT                           gpioPortC
 #define USART0_RX_PIN                            1
@@ -189,11 +185,11 @@
 #define PTI_SPI_DOUT_PORT                        gpioPortC
 #define PTI_SPI_DOUT_PIN                         4
 
-#define PTI_SPI_CS_PORT                          gpioPortC
-#define PTI_SPI_CS_PIN                           5
-
 #define PTI_SPI_CLK_PORT                         gpioPortC
-#define PTI_SPI_CLK_PIN                          6
+#define PTI_SPI_CLK_PIN                          5
+
+#define PTI_SPI_CS_PORT                          gpioPortC
+#define PTI_SPI_CS_PIN                           6
 
 #define XTAL1_PORT                               gpioPortD
 #define XTAL1_PIN                                0
--- Slot_Master/config/sl_board_control_config.h
+++ Slot_Master/config/sl_board_control_config.h
@@ -47,7 +47,7 @@
 
 // <q SL_BOARD_DISABLE_MEMORY_SPI> Disable SPI Flash
 // <i> Default: 1
-#define SL_BOARD_DISABLE_MEMORY_SPI             0
+#define SL_BOARD_DISABLE_MEMORY_SPI             1
 
 // <<< end of configuration section >>>
 
