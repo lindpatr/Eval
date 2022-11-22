368693b1c20448ce849d010a037b68718a050f86
Alex <alexandre.lechenne@belenoscleanpower.com>
2022-11-22 08:25:36 +0100

    [Slot Common] Set default TX power to -30 dBm instead -60

--- Slot_Common/common_config.h
+++ Slot_Common/common_config.h
@@ -76,7 +76,7 @@
 
 #define TX_POWER_MIN    (-900)                  // in dBm (-90 dB)
 #define TX_POWER_MAX    (60)                    // in dBm (60 = 6 dB)
-#define TX_POWER_DEF    (-600)                  // -60 dB
+#define TX_POWER_DEF    (-300)                  // -30 dB
 #define TX_POWER        TX_POWER_DEF            // Default value
 
 #define RAD_BIT_SHIT    (7)
